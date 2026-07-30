626ccbea941c632f54a614dea8060e12da1a40c3
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2026-07-30 08:07:31 +0200

    Fix

--- CreatePackages.bat
+++ CreatePackages.bat
@@ -7,7 +7,9 @@ del nupkg\*.snupkg
 dotnet pack AsyncLock.slnx -c %Configuration% ^
     -p:Version=%PackageVersion% ^
     -p:FileVersion=%PackageVersion% ^
-    -p:AssemblyVersion=%PackageVersion%
+    -p:AssemblyVersion=%PackageVersion% ^
+    -p:DebugType=embedded
+
 
 dotnet pack AsyncLock.slnx -c %Configuration% ^
     -p:Version=%PackageVersion% ^
