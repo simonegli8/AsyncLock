465ae4e915a1b6c7ef2ebb47116f25d85728f63d
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2026-07-30 12:16:48 +0200

    Fix

--- PushToNuget.bat
+++ PushToNuget.bat
@@ -5,5 +5,8 @@ cd nupkg
 for /r %%i in (*.nupkg) do (
     dotnet nuget push %%i --api-key %ApiKey% -s https://api.nuget.org/v3/index.json --skip-duplicate
 )
+for /r %%i in (*.snupkg) do (
+    dotnet nuget push %%i --api-key %ApiKey% -s https://api.nuget.org/v3/index.json --skip-duplicate
+)
 
 cd ..
